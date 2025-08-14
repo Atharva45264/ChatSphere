417481411814c36d9da90eff0c084e4901513a1b
Atharva45 <atharvaphanse403@gmail.com>
2025-08-14 13:27:27 +0530

    setting up controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -34,3 +34,31 @@ export const signup = async (req, res)=>{
     }
 
 }
+
+//Controller to login a user
+
+export const login = async(req, res)=>{
+    try {
+        const {email, password} = req.body;
+        const userData = await User.findOne({email})
+
+        const isPasswordCorrect = await bcrypt.compare(password, userData.password);
+
+        if(!isPasswordCorrect){
+            return res.json({success: false, message: "Invalid credentials"});
+        }
+
+        const token = generateToken(userData._id)
+
+        res.json({success: true, userData, token, message: "Login successfully"})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})       
+    }
+}
+
+// Controller to check if user is authenticated
+export const checkAuth = (req, res)=>{
+    res.json({success: true, user: req.user});
+}
