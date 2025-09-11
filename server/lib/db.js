3239b75699752b578d6e242e6dc4cfc353b98042
Atharva45 <atharvaphanse403@gmail.com>
2025-09-11 13:20:51 +0530

    Completed

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,16 +1,11 @@
 import mongoose from "mongoose";
 
-//Function to connect to mongodb database
-export const connectDB = async () => {
+// Function to connect to the mongodb database
+export const connectDB = async () =>{
     try {
-
-        mongoose.connection.on('connected', ()=> console.log("Databse connected"));
-
-
-        await mongoose.connect(`${process.env.MONGODB_URI}/chat app`)
+        mongoose.connection.on('connected', ()=> console.log('Database Connected'));
+       await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`) 
     } catch (error) {
         console.log(error);
-        
     }
 }
-
--- server/models/User.js
+++ server/models/User.js
@@ -4,9 +4,8 @@ const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     fullName: {type: String, required: true},
     password: {type: String, required: true, minlength: 6},
-    password: {type: String, required: true, minlength: 6},
-    profilePic: {type: String, default:""},
-    password: {type: String},
+    profilePic: {type: String, default: ""},
+    bio: {type: String},
 }, {timestamps: true});
 
 const User = mongoose.model("User", userSchema);
