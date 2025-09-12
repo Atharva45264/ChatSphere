911c94bcc322ae16799eed4f44592f6c36488655
Atharva45 <atharvaphanse403@gmail.com>
2025-09-12 23:17:45 +0530

    code update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/HomePage.jsx'
+import HomePage from '../src/pages/HomePage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
 import { Toaster } from "react-hot-toast"
