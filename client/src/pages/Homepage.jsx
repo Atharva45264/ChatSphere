39f029ab5f533372e1dca164e9fad9675ebcf8d8
Atharva45 <atharvaphanse403@gmail.com>
2025-09-12 23:09:42 +0530

    commit

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
